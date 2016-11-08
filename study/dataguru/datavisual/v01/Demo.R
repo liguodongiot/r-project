f485a656809e6879b7dca43e05298b549fbedcc7
liguodong <liguodongiot@163.com>
2016-11-08 08:14:22 +0800

    数据分析与R语言 第一章和第二章

--- study/dataguru/datavisual/v01/Demo.R
+++ study/dataguru/datavisual/v01/Demo.R
@@ -1,404 +1,235 @@
 
-# 综合性例子
-# 模拟产生统计专业同学的名单（学号区分），记录数学分析，
-#线性代数，概率统计三科成绩，然后进行一些统计分析。
+#矩阵和向量
+#c(), length(), mode(),rbind(),cbind()
 
-#模拟学号
-num=seq(10378001,10378100)
-num
-
-#模拟成绩
-# runif 产生均匀分布的随机数
-x1=round(runif(100,min = 80,max = 100))
+x1 = c(2,4,6,8,0)
+x2 = c(1,3,5,7,9)
+length(x1)
+mode(x1)
 x1
+x1[3]
+
+a1=c(1:100)
+length(a1)
+
+#行
+rbind(x1,x2)
+
+m1=rbind(x1,x2)
+m1
+
+#列
+cbind(x1,x2)
+
+#求平均值，和，连乘，最值，方差，标准差
+
+#函数mean sum min max var sd prod
+x=c(1:100)
+mean(x)
+sum(x)
+max(x)
+min(x)
+#方差
+var(x)
+#连乘
+prod(x)
+#标准差
+sd(x)
 
 
-# rnorm 产生正态分布的随机数
-x2 <- round(rnorm(100,mean = 80,sd = 7))
-x2
+#产生向量
+1:10
+1:10-1
+1:10*2
+2:60*2+1
 
-# 正态分布的产生的随机数没办法指定上下限
-x3 <- round(rnorm(100,mean = 83,sd = 18))
-x3
+a=2:60*2+1
+a
+a[5]
+a[-5]
 
-#将超过100分的成绩变成100分
-x3[which(x3>100)]=100
-x3
+a[1:5]
+a[-(1:5)]
+a[c(2,4,7)]
+a[3:8]
+a[a<20]
+a[a>30 & a<50]
+a[a[3]]
 
-#合成数据框并保存到硬盘
-#data.frame()
-#write.table()
+#seq()
 
-x=data.frame(num,x1,x2,x3)
+seq(5,20)
+seq(5,121,by=2)
+seq(5,121,length=10) #分成10等分
+
+#产生字母序列letters
+letters[1:30]
+
+#which()
+a=c(2,3,4,2,5,1,6,3,2,5,8,5,7,3)
+which.max(a)
+a[which.max(a)]
+
+which(a==2)
+a[which(a==2)]
+
+which(a>5)
+a[which(a>5)]
+
+#反转，排序
+#rev()，sort()
+a=1:20
+a
+
+rev(a)
+a=c(2,3,4,2,5,1,6,3,2,5,8,5,7,3)
+sort(a)
+rev(sort(a))
+
+#生成矩阵
+#matrix()
+a1=c(1:12)
+matrix(a1,nrow=3,ncol=4)
+matrix(a1,nrow=4,ncol=3)
+matrix(a1,nrow=4,ncol=3,byrow=T)
+
+#矩阵运算
+#函数t()，矩阵加减 
+a=matrix(1:12,nrow=3,ncol=4)
+a
+t(a)
+a=b=matrix(1:12,nrow=3,ncol=4)
+a+b
+a-b
+
+#矩阵相乘，函数diag()
+a=matrix(1:12,nrow=3,ncol=4)
+b=matrix(1:12,nrow=4,ncol=3)
+a%*%b
+
+a=matrix(1:16,nrow=4,ncol=4)
+a
+#取出方形矩阵中的对角线
+diag(a)
+# 生成以向量diag(a)为对角线，其他值为0的方形矩阵
+diag(diag(a))
+#生成节数为制定数值的向量
+diag(4)
+
+
+
+#矩阵求逆，rnorm(),solve()
+a=matrix(rnorm(16),4,4)
+a
+
+solve(a)
+
+#解线性方程组
+#函数solve(a,b)
+a=matrix(rnorm(16),4,4)
+a
+
+b=c(1:4)
+b
+
+solve(a,b)
+
+#矩阵的特征值与特征向量
+#eigen
+
+a=diag(4)+1
+a
+
+a.e=eigen(a,symmetric=T)
+a.e
+
+#t() 矩阵的转置
+a.e$vectors%*%diag(a.e$values)%*%t(a.e$vectors)
+
+################
+#数据的R语言表示-数据框
+
+#矩阵形式，但列可以不同数据类型
+#每列是一个变量，每行是一个观测值
+
+x1=c(10,13,45,26,23,12,24,78,23,43,31,56)
+x2=c(20,65,32,32,27,87,60,13,42,51,77,35)
+x=data.frame(x1,x2)
 x
 
-write.table(x,file = "d:\\mark.txt",col.names = F,row.names = F,sep=" ")
+(x=data.frame('重量'=x1,'运费'=x2))
 
+#散点图plot
+plot(x)
 
-#计算各科平均分
-#函数mean(),colMeans(),apply()
 
-#mean(x)  mean(<data.frame>) 已经过时了
+#读取文本数据
+#先设置工作目录，把文本文件放于该目录下
+setwd("D:\\")
+getwd()
+(x=read.table("mark.txt"))
 
-colMeans(x)
+#读剪贴板
+#文本或excel的数据均可通过剪贴板操作
+y <- read.table("clipboard",header=F)
+y
 
-colMeans(x)[c("x1","x2","x3")]
+z <- read.table("clipboard",header=T)
+z
 
-apply(x, 2, mean)
+#读excel文件数据
+#方法1：先把excel另存为空格分隔的prn文本格式再读
+w <- read.table("d:\\test.prn",header=F)
+w
 
-#求最高最低分
-apply(x,2,max)
-apply(x,2,min)
 
-#求每个人的总分
-# 1表示行 2表示列
-apply(x[c("x1","x2","x3")],1,sum)
+#方法2：安装RODBC,再通过ODBC读
+install.packages("RODBC")
+library(RODBC)
 
-#总分最高的同学
-#先找到下标，再根据下标找到学号
-which.max(apply(x[c("x1","x2","x3")], 1, sum))
+local({pkg<-select.list(sort(.packages(all.available = TRUE)),graphics = TRUE) 
+    if(nchar(pkg)) library(pkg,character.only = TRUE)})
 
-x$num[which.max(apply(x[c("x1","x2","x3")], 1, sum))]
+#注：xlsx不能被读取
+z <- odbcConnectExcel("d:\\test.xls")
+(w<-sqlFetch(z,"Sheet1",colnames = FALSE))
 
 
+#循环语句
+#for
+for(i in 1:59) {a[i]=i*2+3}
+a
 
-####################
-#图表分析
 
-#对x1进行直方图分析
-#hist
-hist(x$x1)
+b=0
+for(i in 1:59) {a[i]=i*2+3;b[i]=i*5+4}
+b
 
-#探索各科成绩的关联关系
-#散点图plot()
-plot(x1,x2)
-plot(x$x1,x$x2)
 
-#列链表分析
-#列联函数table()，柱状图绘制函数barplot()
-x$x1
-table(x$x1)
-barplot(table(x$x1))
 
-#饼图
-#饼图绘制函数pie()
-pie(table(x$x1))
+#while
+a[1]=5
+i=1
+while(a[i]<121) {i=i+1;a[i]=a[i-1]+2}
+a
 
-#箱尾图（箱线图）
-#箱子的上线横线为样本的25%和75%的分位数
-#箱子中间的横线为样本的中位数
-#上下延伸的直线称为尾线，尾线的尽头为最高值和最低值
-#异常值
-boxplot(x$x1,x$x2,x$x3)
 
-#指定了颜色和缺口
-boxplot(x[2:4],col=c("red","green","blue"),notch = T)
 
-#水平放置
-boxplot(x$x1,x$x2,x$x3,horizontal = T)
+#R脚本
+#source()函数
+#print()函数
+#绝对路径
+source("d:\\test.r")
 
+#相对路径
+source("study\\dataguru\\datavisual\\v01\\test.r")
 
-#星相图
-#每个观测单位的数值表示为一个图形
-#每个图的每个角表示一个变量，字符串类型会标注在图的下方
-#角线的长度表达值的大小
-stars(x[c("x1","x2","x3")])
 
-#雷达图
-stars(x[c("x1","x2","x3")],full=T,draw.segments = T)
-
-#full=F 表示画半圆 T表示全圆
-stars(x[c("x1","x2","x3")],full=F,draw.segments = T)
-
-#脸谱图
-install.packages("aplpack")
-library(aplpack)
-
-#用五官的宽度和高度来描绘数值
-#人对脸谱高度敏感和强记忆
-#适合较少样本的情况
-faces(x[c("x1","x2","x3")])
-
-#其他脸谱图
-install.packages("TeachingDemos")
-library(TeachingDemos)
-faces2(x)
-faces(x[c("x1","x2","x3")])
-
-#茎叶图
-x$x1
-table(x$x1)
-
-stem(x$x1)
-
-
-#QQ图
-#可用于判断是否是正态分布
-#直线的斜率是标准差，截距是均值
-#点的散步越接近直线，则越接近正态分布
-qqnorm(x1)
-qqline(x1)
-
-qqnorm(x3)
-qqline(x3)
-
-
-# 散点图
-
-plot(x$x1,x$x2,main="数学分析与线性代数成绩的关系",
-     xlab = "数学分析",ylab = "线性代数",
-     xlim=c(60,100),ylim=c(60,100),
-     xaxs="i", # Set x axis style as internal(i表示内置的风格)
-     yaxs="i", # Set y axis style as internal
-     col="red", # Set the color of plotting symbol to red
-     pch=19 # Set the plotting symbol to filled dots
-     )
-
-#连线图
-a=c(2,3,4,5,6)
-b=c(4,7,8,9,12)
-plot(a,b,type="l") #l表示线，p表示点...
-
-#多条曲线的效果
-
-Tokyo=c(200,250,230,270,700,400,500,300,350,220,180,210)
-NewYork=c(100,210,230,270,400,500,400,350,330,220,210,230)
-London=c(150,270,230,270,300,400,600,200,250,220,190,200)
-Berlin=c(500,470,330,270,200,230,220,240,250,270,300,400)
-
-rain=data.frame(Tokyo,NewYork,London,Berlin)
-
-plot(rain$Tokyo,type = "l",col="red",
-     ylim = c(0,1000),
-     main="Monthly Rainfall in major cities",
-     xlab="Month of Year",
-     ylab="Rainfall(mm)",
-     lwd=2)
-lines(rain$NewYork,type="l",col="blue",lwd=2)
-lines(rain$London,type="l",col="green",lwd=2)
-lines(rain$Berlin,type="l",col="orange",lwd=2)
-
-
-#密度图
-#density
-
-plot(density(rnorm(1000)))
-
-#########################
-#R内置数据集
-
-#列出内置数据
-data()
-
-#汽车数据集
-mtcars
-
-#热力图
-heatmap(as.matrix(mtcars),
-        Rowv=NA,
-        Colv=NA,
-        col=heat.colors(256),
-        scale="column",
-        margins = c(2,8),
-        main="Car characteristics by Model")
-
-#鸢尾花数据集iris
-#Sepal 花萼
-#Petal 花瓣
-#Species 种属
-
-iris
-
-
-#向日葵散点图
-#用来克服散点图中数据点重叠问题
-#在有重叠的地方用一朵“向日葵花”的花瓣数目来表示重叠数据的个数
-
-sunflowerplot(iris[,3:4],
-              col="gold",
-              seg.col="red")
-
-#散点图集
-#遍历样本中全部的变量配对 画出二元图 2^4
-#直观地了解所有变量之间的关系
-
-pairs(iris[,1:4])
-
-#用plot实现同样的效果
-
-plot(iris[,1:4],
-     main="Relationships between characteristics of iris flowers",
-     pch=19,
-     col="blue",
-     cex=0.9)
-
-# 利用par()在同一个device输出多个散点图
-#par命令博大精深，用于设置绘图参数.help(par)
-
-op <- par(no.readonly = FALSE) 
-
-par(mfrow=c(3,1))
-
-plot(x1,x2);plot(x2,x3);plot(x3,x1)
-
-par(op)
-
-######################
-#关于绘图参数
-help(par)
-
-#颜色
-colors()
-
-#绘图设备
-# dev.cur() 当前图形窗口
-# dev.list() 有哪些图形窗口
-# dev.next(which=dev.cur())
-# dev.prev(which=dev.cur())
-# dev.off(which=dev.cur())
-# dev.set(which=dev.next())
-# dev.new(...) 新建图形窗口
-# graphics.off() 
-x1 <- c(1,2,3)
-x2 <- c(4,3,2)
-plot(x1,x2)
-x3=c(3,4,4,5)
-x4=c(5,4,9,7)
-plot(x3,x4)
-
-#多个图形窗共存？
-dev.new()
-plot(x1,x2)
-
-dev.list()
-dev.cur() 
-dev.next()
-dev.cur() 
-
-
-#三维散点图
-
-install.packages("scatterplot3d")
-library(scatterplot3d)
-
-scatterplot3d(x[2:4])
-
-#
-
-x<-y<-seq(-2*pi,2*pi,pi/15)
-
-f<-function(x,y) sin(x)*sin(y)
-
-z <- outer(x,y,f)
-
-contour(x,y,z,col="blue")
-
-persp(x,y,z,
-      theta=30,
-      phi=30,
-      expand=0.7,
-      col="lightblue")
-
-#调和曲线图
-#调和曲线图用于聚类判断非常方便。
-
-#地图
-install.packages("maps")
-library(maps)
-
-map("state",interior=FALSE)
-
-map("state",
-    boundary=FALSE,
-    col="red",
-    add=TRUE)
-
-map('world',fill=TRUE,col=heat.colors(10))
-
-
-#社交数据可视化
-install.packages("geosphere")
-library(maps)
-library(geosphere)
-
-#画美国地图
-map("state")
-
-#画世界地图
-map("world")
-
-#通过设置坐标范围使
-#焦点集中在美国周边，并且设置一些有关颜色
-xlim<-c(-171.738281,-56.601563)
-ylim<-c(12.039321,71.856229)
-
-map("world",
-    col="#f2f2f2",
-    fill=TRUE,
-    bg="white",
-    lwd=0.05,xlim=xlim,
-    ylim=ylim
-    )
-
-
-#画一条弧线表示社交关系
-lat_ca<-39.164141
-lon_ca<- -121.640625
-lat_me <- 45.213004
-lon_me <- -68.906250
-inter <- gcIntermediate(
-  c(lon_ca,lat_ca),
-  c(lon_me,lat_me),
-  n=50,
-  addStartEnd=TRUE)
-
-lines(inter)
-
-#继续画弧线
-lat_tx <- 29.954935
-lon_tx <- -98.01172
-inter2 <- gcIntermediate(
-  c(lon_ca,lat_ca),
-  c(lon_tx,lat_tx),
-  n=50,
-  addStartEnd=TRUE
-)
-lines(inter2,col="red")
-
-
-#装载数据
-airports <- read.csv("http://datasets.flowingdata.com/tuts/maparcs/airports.csv",
-                     header=TRUE)
-
-flights <- read.csv("http://datasets.flowingdata.com/tuts/maparcs/flights.csv",
-                   header=TRUE,as.is=TRUE)
-
-#画出多重联系
-
-map("world",col="#f2f2f2",
-    fill=TRUE,bg="white",lwd=0.05,
-    xlim=xlim,ylim=ylim)
-
-fsub<-flights[flights$airline=="AA",]
-
-for(j in 1:length(fsub$airline)){
-  air1 <- airports[airports$iata == fsub[j,]$airport1,]
-  air2 <- airports[airports$iata == fsub[j,]$airport2,]
-  
-  inter3 <- gcIntermediate(
-      c(air1[1,]$long,air1[1,]$lat),
-      c(air2[1,]$long,air2[1,]$lat),
-      n=100,
-      addStartEnd=TRUE
-    )
-  lines(inter3,col="black",lwd=0.8)
-}
-
-
-
-
-
-
-
-
-
+#分布函数
+#正态分布函数rnorm()
+#泊松分布函数rpois()
+#指数分布函数rexp()
+#Gamma分布函数rgamma()
+#均匀分布函数runif()
+#二项分布函数rbinom()
+#几何分布函数rgeom()
